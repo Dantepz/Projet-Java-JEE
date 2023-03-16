35e6548e1e340c80a955641c033a9dce6cdb3b41
Valentin Leclerc <valmout2001@gmail.com>
2023-03-16 11:59:16 +0100

    Ajout des informations des mairies + lecture des XML

---
+++ Equipements_Sportifs/src/main/Java/fr/esigelec/jee/Adresse.java
@@ -0,0 +1,10 @@
+package fr.esigelec.jee;
+
+public class Adresse {
+    private String ligne;
+    private int codePostal;
+    private String Commune;
+    private double latitude;
+    private double longitude;
+    private double precision;
+}
---
+++ Equipements_Sportifs/src/main/Java/fr/esigelec/jee/Coordonnees.java
@@ -0,0 +1,7 @@
+package fr.esigelec.jee;
+
+public class Coordonnees {
+    private String telephone;
+    private String mail;
+    private String url;
+}
