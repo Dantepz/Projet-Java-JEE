da4fc3c60508472a95352b998e2dbad1eaf3c5bd
Valentin Leclerc <valmout2001@gmail.com>
2023-03-20 16:15:58 +0100

    update

--- Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/DBDAO.java
+++ Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/DBDAO.java
@@ -28,7 +28,7 @@ public class DBDAO {
 
     public static Connection connection;
     public static Statement statement;
-    public PreparedStatement preparedStatement;
+    public static PreparedStatement preparedStatement;
 
     /**
      * Constructor.
@@ -37,60 +37,7 @@ public class DBDAO {
         dbConnect();
     }
 
-    /**
-     * Method used to return the country name from a country code.
-     * @param countryCode the code of a country.
-     * @return The name of the country.
-     */
-    public String getCountryName(String countryCode) {
-        String countryName;
-        switch(countryCode) {
-            case "fr":
-                countryName = "France";
-                break;
-            case "es":
-                countryName = "Spain";
-                break;
-            case "pt":
-                countryName = "Portugal";
-                break;
-            default:
-                countryName = "Country not found";
-                break;
-        }
-        return countryName;
-    }
-
-    /**
-     * Method used to return the capital name from a country code.
-     * @param countryCode the code of a country.
-     * @return the capital of a country with a country code.
-     */
-    public String getCityName(String countryCode) {
-        String countryCity;
-        switch(countryCode) {
-            case "fr":
-                countryCity = "Paris";
-                break;
-            case "es":
-                countryCity = "Madrid";
-                break;
-            case "pt":
-                countryCity = "Lisbon";
-                break;
-            default:
-                countryCity = "City not found";
-                break;
-        }
-        return countryCity;
-    }
-
-    /**
-     * Method used to create a list with the country codes inside of it.
-     * @return ArrayList with the country codes of all the lines in the database.
-     */
-    public ArrayList<String> getCountryCodes() {
-        ArrayList<String> cr = new ArrayList<String>();
+    public void selectStatement() {
         try {
             connection = DriverManager.getConnection(mySQLURL, username, password);
 
@@ -98,49 +45,15 @@ public class DBDAO {
             String query = "SELECT ccode FROM `country_capitals`;";
             ResultSet resultSet = statement.executeQuery(query);
 
-            hashMap = new HashMap<>();
-            while (resultSet.next()) {
-                cr.add(resultSet.getString(1));
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return cr;
-    }
-
-    /**
-     * Method used to connect an user with its login / password and compare it from them from the db.
-     * @param name the login.
-     * @param pass the password.
-     * @return true if connected and false if not.
-     */
-    public boolean validateUserCredentials(String name, String pass) {
-        String sqlUser = "";
-        String sqlPassword = "";
-        pass = hashPassword(pass);
-        try {
-
-            statement = connection.createStatement();
-            String query = "SELECT * FROM `user`"
-                    + "WHERE name='" + name + "' AND password = '" + pass + "';";
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()) {
-                sqlUser = resultSet.getString(2);
-                sqlPassword = resultSet.getString(4);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return sqlUser.equals(name) && sqlPassword.equals(pass);
     }
 
     /**
      * Method used to initialize a connection to the database.
      */
-    private static void dbConnect() {
+    public static void dbConnect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch(ClassNotFoundException cnfe) {
@@ -173,54 +86,6 @@ public class DBDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Method used to hash password before send it in the database.
-     * @param password the clear password that we want to hash.
-     * @return the hashed password.
-     */
-    public String hashPassword(String password) {
-        MessageDigest digest;
-        StringBuilder hexString = new StringBuilder();
-
-        try {
-            digest = MessageDigest.getInstance("SHA-256");
-
-            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
-
-            for (byte b : hash) {
-                String hex = Integer.toHexString(0xff & b);
-                if (hex.length() == 1) {
-                    hexString.append('0');
-                }
-                hexString.append(hex);
-            }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return hexString.toString();
-    }
-
-    /**
-     * Method used to add a new country in the database.
-     * @param country the name of the country.
-     * @param capital the name of the capital.
-     * @return true if ok and false if there is an error.
-     */
-    public boolean addNewCountry(String country, String capital) {
-        boolean state = false;
-        try {
-            preparedStatement = connection.prepareStatement("INSERT INTO `country_capitals` (ccode, cname, ccapital) VALUES (?, ?, ?)");
-            preparedStatement.setString(1, country.substring(0, 2).toLowerCase()); // getting the two firsts characters from the country name to save it as the country code.
-            preparedStatement.setString(2, country);
-            preparedStatement.setString(3, capital);
-
-            state = preparedStatement.executeUpdate() == 1;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return state;
     }
 
     /**
--- Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/MairieDAO.java
+++ Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/MairieDAO.java
@@ -1,25 +1,29 @@
 package fr.esigelec.jee.dao;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.sql.SQLException;
+import java.sql.Types;
 
-public class MairieDAO
+public class MairieDAO extends DBDAO
 {
     public static void main(String[] argv)
     {
         File folder = new File("cleanDatas");
         File[] listOfFiles = folder.listFiles();
+        String codeInsee = "";
+
+        dbConnect();
 
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
                 if (file.isFile()) {
                     System.out.println(file.getName());
+                    String[] strings = file.getName().split("\\.");
+                    codeInsee = strings[0];
                 }
                 try
                 {
@@ -34,13 +38,83 @@ public class MairieDAO
                     // nodeList is not iterable, so we are using for loop
                     for (int itr = 0; itr < nodeList.getLength(); itr++) {
                         Node node = nodeList.item(itr);
-                        System.out.println("\nNode Name :" + node.getNodeName());
                         if (node.getNodeType() == Node.ELEMENT_NODE) {
                             Element eElement = (Element) node;
-                            System.out.println("Nom: "+ eElement.getElementsByTagName("Nom").item(0).getTextContent());
-                            System.out.println("Adresse: "+ eElement.getElementsByTagName("Adresse").item(0).getTextContent());
-                            System.out.println("Coordonnées Numériques: "+ eElement.getElementsByTagName("CoordonnéesNum").item(0).getTextContent());
-                            System.out.println("Horaires d'ouverture: "+ eElement.getElementsByTagName("Ouverture").item(0).getTextContent());
+                            try {
+                                // Mairies
+//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie` (mairie_id, mairie_nom) VALUES (?, ?)");
+//                                preparedStatement.setString(1, codeInsee);
+//                                preparedStatement.setString(2, eElement.getElementsByTagName("Nom").item(0).getTextContent());
+//                                System.out.println(preparedStatement.toString());
+//                                /*if(preparedStatement.executeUpdate() != 1)
+//                                    // TODO : print error
+//                                    System.out.println("error");*/
+//
+//                                // Adresses
+//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_adresse` " +
+//                                        "(mairie_adresse.adresse_id, " +
+//                                        "mairie_adresse.adresse_ligne, " +
+//                                        "mairie_adresse.adresse_codePostal, " +
+//                                        "mairie_adresse.adresse_commune, " +
+//                                        "mairie_adresse.adresse_latitude, " +
+//                                        "mairie_adresse.adresse_longitude, " +
+//                                        "mairie_adresse.adresse_precision) " +
+//                                        "VALUES (?, ?, ?, ?, ?, ?, ?)");
+//                                preparedStatement.setString(1, codeInsee);
+//                                preparedStatement.setString(2, eElement.getElementsByTagName("Ligne").item(0).getTextContent());
+//                                preparedStatement.setString(3, eElement.getElementsByTagName("CodePostal").item(0).getTextContent());
+//                                preparedStatement.setString(4, eElement.getElementsByTagName("NomCommune").item(0).getTextContent());
+//                                preparedStatement.setString(5, eElement.getElementsByTagName("Latitude").item(0).getTextContent());
+//                                preparedStatement.setString(6, eElement.getElementsByTagName("Longitude").item(0).getTextContent());
+//                                preparedStatement.setString(7, eElement.getElementsByTagName("Précision").item(0).getTextContent());
+//                                System.out.println(preparedStatement.toString());
+//                                /*if(preparedStatement.executeUpdate() != 1)
+//                                    // TODO : print error
+//                                    System.out.println("error");*/
+//
+//                                // Coordonnées
+//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_coordonnees` " +
+//                                        "(mairie_coordonnees.coordonnees_id, " +
+//                                        "mairie_coordonnees.coordonnees_telephone," +
+//                                        "mairie_coordonnees.coordonnees_mail," +
+//                                        "mairie_coordonnees.coordonnees_url) " +
+//                                        "VALUES (?, ?, ?, ?)");
+//                                preparedStatement.setString(1, codeInsee);
+//                                preparedStatement.setString(2, eElement.getElementsByTagName("Téléphone").item(0).getTextContent());
+//                                preparedStatement.setString(3, eElement.getElementsByTagName("Email").item(0).getTextContent());
+//                                if(eElement.getElementsByTagName("Url").item(0).getTextContent() == null)
+//                                    preparedStatement.setString(4, eElement.getElementsByTagName("Url").item(0).getTextContent());
+//                                else
+//                                    preparedStatement.setNull(4, Types.VARCHAR);
+//                                System.out.println(preparedStatement.toString());
+//                                /*if(preparedStatement.executeUpdate() != 1)
+//                                    // TODO : print error
+//                                    System.out.println("error");*/
+
+                                // Ouverture
+                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_ouverture` " +
+                                        "(mairie_ouverture.ouverture_id, " +
+                                        "mairie_ouverture.ouverture_plageJ_debut," +
+                                        "mairie_ouverture.ouverture_plageJ_fin," +
+                                        "mairie_ouverture.ouverture_plageH_debut," +
+                                        "mairie_ouverture.ouverture_plageH_fin) " +
+                                        "VALUES (?, ?, ?, ?, ?)");
+                                preparedStatement.setString(1, codeInsee);
+                                NamedNodeMap namedNodeMap = eElement.getElementsByTagName("Téléphone").item(0).getAttributes();
+                                /*preparedStatement.setString(2, eElement.getAttribute("début"));
+                                preparedStatement.setString(3, eElement.getElementsByTagName("Email").item(0).getTextContent());
+                                if(eElement.getElementsByTagName("Url").item(0).getTextContent() == null)
+                                    preparedStatement.setString(4, eElement.getElementsByTagName("Url").item(0).getTextContent());
+                                else
+                                    preparedStatement.setString(4, "");*/
+                                System.out.println(preparedStatement.toString());
+                                /*if(preparedStatement.executeUpdate() != 1)
+                                    // TODO : print error
+                                    System.out.println("error");*/
+
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 } catch (Exception e) {
@@ -48,5 +122,6 @@ public class MairieDAO
                 }
             }
         }
+        dbClose();
     }
 }  
