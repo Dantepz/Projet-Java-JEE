6d29e24b3129ac23f3a6e03bd5ecae30fba1aa3f
Valentin Leclerc <valmout2001@gmail.com>
2023-03-20 18:42:40 +0100

    Ajout des insert pour les tables concernan les mairies Attention : cela ne fonctionne pas encore donc il ne faut pas s'en servir de suite pour ne pas polluer la base.

--- Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/MairieDAO.java
+++ Equipements_Sportifs/src/main/Java/fr/esigelec/jee/dao/MairieDAO.java
@@ -1,11 +1,12 @@
 package fr.esigelec.jee.dao;
 
-import org.w3c.dom.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
 import java.io.File;
 import java.sql.SQLException;
 import java.sql.Types;
@@ -41,65 +42,64 @@ public class MairieDAO extends DBDAO
                         if (node.getNodeType() == Node.ELEMENT_NODE) {
                             Element element = (Element) node;
                             // Mairies
-//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie` (mairie_id, mairie_nom) VALUES (?, ?)");
-//                                preparedStatement.setString(1, codeInsee);
-//                                preparedStatement.setString(2, element.getElementsByTagName("Nom").item(0).getTextContent());
-//                                System.out.println(preparedStatement.toString());
-//                                /*if(preparedStatement.executeUpdate() != 1)
-//                                    // TODO : print error
-//                                    System.out.println("error");*/
-//
-//                                // Adresses
-//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_adresse` " +
-//                                        "(mairie_adresse.adresse_id, " +
-//                                        "mairie_adresse.adresse_ligne, " +
-//                                        "mairie_adresse.adresse_codePostal, " +
-//                                        "mairie_adresse.adresse_commune, " +
-//                                        "mairie_adresse.adresse_latitude, " +
-//                                        "mairie_adresse.adresse_longitude, " +
-//                                        "mairie_adresse.adresse_precision) " +
-//                                        "VALUES (?, ?, ?, ?, ?, ?, ?)");
-//                                preparedStatement.setString(1, codeInsee);
-//                                preparedStatement.setString(2, element.getElementsByTagName("Ligne").item(0).getTextContent());
-//                                preparedStatement.setString(3, element.getElementsByTagName("CodePostal").item(0).getTextContent());
-//                                preparedStatement.setString(4, element.getElementsByTagName("NomCommune").item(0).getTextContent());
-//                                preparedStatement.setString(5, element.getElementsByTagName("Latitude").item(0).getTextContent());
-//                                preparedStatement.setString(6, element.getElementsByTagName("Longitude").item(0).getTextContent());
-//                                preparedStatement.setString(7, element.getElementsByTagName("Précision").item(0).getTextContent());
-//                                System.out.println(preparedStatement.toString());
-//                                /*if(preparedStatement.executeUpdate() != 1)
-//                                    // TODO : print error
-//                                    System.out.println("error");*/
-//
-//                                // Coordonnées
-//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_coordonnees` " +
-//                                        "(mairie_coordonnees.coordonnees_id, " +
-//                                        "mairie_coordonnees.coordonnees_telephone," +
-//                                        "mairie_coordonnees.coordonnees_mail," +
-//                                        "mairie_coordonnees.coordonnees_url) " +
-//                                        "VALUES (?, ?, ?, ?)");
-//                                preparedStatement.setString(1, codeInsee);
-//                                preparedStatement.setString(2, element.getElementsByTagName("Téléphone").item(0).getTextContent());
-//                                preparedStatement.setString(3, element.getElementsByTagName("Email").item(0).getTextContent());
-//                                if(element.getElementsByTagName("Url").item(0).getTextContent() == null)
-//                                    preparedStatement.setString(4, element.getElementsByTagName("Url").item(0).getTextContent());
-//                                else
-//                                    preparedStatement.setNull(4, Types.VARCHAR);
-//                                System.out.println(preparedStatement.toString());
-//                                /*if(preparedStatement.executeUpdate() != 1)
-//                                    // TODO : print error
-//                                    System.out.println("error");*/
-//
-//                                // Ouverture
-//                                preparedStatement = connection.prepareStatement("INSERT INTO `mairie_ouverture` " +
-//                                        "(mairie_ouverture.ouverture_id, " +
-//                                        "mairie_ouverture.ouverture_plageJ_debut," +
-//                                        "mairie_ouverture.ouverture_plageJ_fin," +
-//                                        "mairie_ouverture.ouverture_plageH_debut," +
-//                                        "mairie_ouverture.ouverture_plageH_fin) " +
-//                                        "VALUES (?, ?, ?, ?, ?)");
-//                                preparedStatement.setString(1, codeInsee);
-//                                preparedStatement.setString(2, element.getAttribute("début"));
+                            preparedStatement = connection.prepareStatement("INSERT INTO `mairie` (mairie_id, mairie_nom) VALUES (?, ?)");
+                            preparedStatement.setString(1, codeInsee);
+                            preparedStatement.setString(2, element.getElementsByTagName("Nom").item(0).getTextContent());
+                            System.out.println(preparedStatement.toString());
+                            /*if(preparedStatement.executeUpdate() != 1)
+                                // TODO : print error
+                                System.out.println("error");*/
+
+                            // Adresses
+                            preparedStatement = connection.prepareStatement("INSERT INTO `mairie_adresse` " +
+                                    "(mairie_adresse.adresse_id, " +
+                                    "mairie_adresse.adresse_ligne, " +
+                                    "mairie_adresse.adresse_codePostal, " +
+                                    "mairie_adresse.adresse_commune, " +
+                                    "mairie_adresse.adresse_latitude, " +
+                                    "mairie_adresse.adresse_longitude, " +
+                                    "mairie_adresse.adresse_precision) " +
+                                    "VALUES (?, ?, ?, ?, ?, ?, ?)");
+                            preparedStatement.setString(1, codeInsee);
+                            preparedStatement.setString(2, element.getElementsByTagName("Ligne").item(0).getTextContent());
+                            preparedStatement.setString(3, element.getElementsByTagName("CodePostal").item(0).getTextContent());
+                            preparedStatement.setString(4, element.getElementsByTagName("NomCommune").item(0).getTextContent());
+                            preparedStatement.setString(5, element.getElementsByTagName("Latitude").item(0).getTextContent());
+                            preparedStatement.setString(6, element.getElementsByTagName("Longitude").item(0).getTextContent());
+                            preparedStatement.setString(7, element.getElementsByTagName("Précision").item(0).getTextContent());
+                            System.out.println(preparedStatement.toString());
+                            /*if(preparedStatement.executeUpdate() != 1)
+                                // TODO : print error
+                                System.out.println("error");*/
+
+                            // Coordonnées
+                            preparedStatement = connection.prepareStatement("INSERT INTO `mairie_coordonnees` " +
+                                    "(mairie_coordonnees.coordonnees_id, " +
+                                    "mairie_coordonnees.coordonnees_telephone," +
+                                    "mairie_coordonnees.coordonnees_mail," +
+                                    "mairie_coordonnees.coordonnees_url) " +
+                                    "VALUES (?, ?, ?, ?)");
+                            preparedStatement.setString(1, codeInsee);
+                            preparedStatement.setString(2, element.getElementsByTagName("Téléphone").item(0).getTextContent());
+                            preparedStatement.setString(3, element.getElementsByTagName("Email").item(0).getTextContent());
+                            if(element.getElementsByTagName("Url").item(0).getTextContent() == null)
+                                preparedStatement.setString(4, element.getElementsByTagName("Url").item(0).getTextContent());
+                            else
+                                preparedStatement.setNull(4, Types.VARCHAR);
+                            System.out.println(preparedStatement.toString());
+                            /*if(preparedStatement.executeUpdate() != 1)
+                                // TODO : print error
+                                System.out.println("error");*/
+
+                            // Ouverture
+                            preparedStatement = connection.prepareStatement("INSERT INTO `mairie_ouverture` " +
+                                    "(mairie_ouverture.ouverture_id, " +
+                                    "mairie_ouverture.ouverture_plageJ_debut," +
+                                    "mairie_ouverture.ouverture_plageJ_fin," +
+                                    "mairie_ouverture.ouverture_plageH_debut," +
+                                    "mairie_ouverture.ouverture_plageH_fin) " +
+                                    "VALUES (?, ?, ?, ?, ?)");
+                            preparedStatement.setString(1, codeInsee);
 
                             DocumentBuilderFactory docBuildOuverture = DocumentBuilderFactory.newInstance();
                             DocumentBuilder documentBuilderOuverture = docBuild.newDocumentBuilder();
@@ -112,18 +112,19 @@ public class MairieDAO extends DBDAO
                                 if(nodeOuverture.getNodeType() == Node.ELEMENT_NODE) {
                                     Element elementOuverture = (Element) nodeOuverture;
                                     try {
-                                        if(element.getElementsByTagName("PlageJ").item(0).getTextContent() != null)
+                                        if(element.getElementsByTagName("PlageJ").item(0).getTextContent() != null) {
+                                            preparedStatement.setString(2, element.getAttribute("début"));
                                             preparedStatement.setString(3, element.getElementsByTagName("PlageJ").item(0).getTextContent());
+                                        }
                                     } catch (SQLException e) {
                                         e.printStackTrace();
                                     }
                                 }
                             }
                             System.out.println(preparedStatement.toString());
-                                /*if(preparedStatement.executeUpdate() != 1)
-                                    // TODO : print error
-                                    System.out.println("error");*/
-
+                            /*if(preparedStatement.executeUpdate() != 1)
+                                // TODO : print error
+                                System.out.println("error");*/
                         }
                     }
                 } catch (Exception e) {
--- Equipements_Sportifs/src/main/webapp/Page.jsp
+++ Equipements_Sportifs/src/main/webapp/Page.jsp
@@ -1,14 +1,17 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>test</h1>
-<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=1.0792779922485354%2C49.3770642379392%2C1.1360979080200198%2C49.396591284851276&amp;layer=mapnik" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/#map=15/49.3868/1.1077">View Larger Map</a></small>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>test</h1>
+        <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=1.0792779922485354%2C49.3770642379392%2C1.1360979080200198%2C49.396591284851276&amp;layer=mapnik" style="border: 1px solid black">
+        </iframe>
+        <br/>
+        <small><a href="https://www.openstreetmap.org/#map=15/49.3868/1.1077">View Larger Map</a></small>
+    </body>
 </html>
